696ab5ffa312b7ccaec41d85bd1c4d422078dd52
kouki <kouworld0123@gmail.com>
2017-08-04 04:54:16 +0000

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
+    # debugger # ブレイクポイント作って鯖起動中のコンソールで現在の情報を確認できる。
   end
+  
+  def new
+    @user = User.new
+    # debugger
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if 
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
 end
