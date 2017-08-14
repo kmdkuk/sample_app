a846b0dec9ad2ee2c564ec1ac0e41c9fa9a61489
kouki <kouworld0123@gmail.com>
2017-08-14 16:03:03 +0000

    add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,11 +52,18 @@ class User < ApplicationRecord
   
   def create_reset_digest
     self.reset_token = User.new_token
+    # update_attribute(:reset_digest,  User.digest(reset_token))
+    # update_attribute(:reset_sent_at, Time.zone.now)
     update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
   
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+  
+  # パスワード再設定の期限が切れている場合はtrue
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
   
   private
